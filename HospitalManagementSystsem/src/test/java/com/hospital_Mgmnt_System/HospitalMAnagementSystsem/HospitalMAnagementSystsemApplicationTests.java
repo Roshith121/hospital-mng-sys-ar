64fbcd666607c5c466a830f33926ee09f65f7712
anushka-u <anushkau032@gmail.com>
2024-11-22 23:03:35 +0530

    Adding Project Structure.

---
+++ HospitalManagementSystsem/src/test/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/HospitalMAnagementSystsemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital_Mgmnt_System.HospitalMAnagementSystsem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMAnagementSystsemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
