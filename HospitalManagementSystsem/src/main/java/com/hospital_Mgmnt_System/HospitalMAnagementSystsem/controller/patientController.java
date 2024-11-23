6fcd09872c9953fef430a3455cf64b4788ca732b
anushka-u <anushkau032@gmail.com>
2024-11-23 22:42:00 +0530

    Adding CrossOrigin annotation in patientController

--- HospitalManagementSystsem/src/main/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/controller/patientController.java
+++ HospitalManagementSystsem/src/main/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/controller/patientController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hospital_Mgmnt_System.HospitalMAnagementSystsem.entity.Patient;
 import com.hospital_Mgmnt_System.HospitalMAnagementSystsem.repository.PatientRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class patientController {
