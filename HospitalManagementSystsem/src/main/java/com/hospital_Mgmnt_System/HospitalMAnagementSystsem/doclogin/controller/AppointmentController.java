1cc129d22c020840635d14b2a8fdaf5fe0809dcb
anushka-u <anushkau032@gmail.com>
2024-11-23 22:41:19 +0530

    Adding More functionality in Appointment Controller

--- HospitalManagementSystsem/src/main/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/doclogin/controller/AppointmentController.java
+++ HospitalManagementSystsem/src/main/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/doclogin/controller/AppointmentController.java
@@ -1,8 +1,13 @@
 package com.hospital_Mgmnt_System.HospitalMAnagementSystsem.doclogin.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.management.AttributeNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hospital_Mgmnt_System.HospitalMAnagementSystsem.doclogin.entity.Appointment;
 import com.hospital_Mgmnt_System.HospitalMAnagementSystsem.doclogin.repository.AppointmentRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/appointment")
 public class AppointmentController {
@@ -35,9 +45,20 @@ public class AppointmentController {
 		return appointmentRepository.save(appointment);
 	}
 	
-	@GetMapping()
+	@GetMapping("/fetch")
 	public List<Appointment> getAppointments() {
 		return appointmentRepository.findAll();
 	}	
+	
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteAppointment(@PathVariable long id) throws AttributeNotFoundException
+	{
+		Appointment appointment = appointmentRepository.findById(id).orElseThrow(()-> new AttributeNotFoundException("Attribute with" +   id + " doesn't exist"));
+		appointmentRepository.delete(appointment);
+		Map<String, Boolean> response = new HashMap<String, Boolean>();
+		response.put("Deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
 	
 }
