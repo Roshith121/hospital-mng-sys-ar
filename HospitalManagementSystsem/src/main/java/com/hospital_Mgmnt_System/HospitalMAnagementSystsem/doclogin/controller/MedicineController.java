5359719aaf308f01eefcf3c4dfab544d1c35dc8c
anushka-u <anushkau032@gmail.com>
2024-11-23 21:51:49 +0530

    Adding Medicine Controller

--- HospitalManagementSystsem/src/main/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/doclogin/controller/MedicineController.java
+++ HospitalManagementSystsem/src/main/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/doclogin/controller/MedicineController.java
@@ -1,6 +1,9 @@
 package com.hospital_Mgmnt_System.HospitalMAnagementSystsem.doclogin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,10 @@ public class MedicineController {
 	@PostMapping("/meds")
 	public Medicine createMedicine(@RequestBody Medicine medicine) {
 		return medicineRepository.save(medicine);
+	}
+	
+	@GetMapping
+	public List<Medicine> getAllMedicines(){
+		return medicineRepository.findAll();
 	}
 }
