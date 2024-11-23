773232013c17ec6db3cb7e9a8b26b1ea769ba702
anushka-u <anushkau032@gmail.com>
2024-11-23 23:40:49 +0530

    Adding more functionality in Patient Controller

--- HospitalManagementSystsem/src/main/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/controller/patientController.java
+++ HospitalManagementSystsem/src/main/java/com/hospital_Mgmnt_System/HospitalMAnagementSystsem/controller/patientController.java
@@ -1,14 +1,22 @@
 package com.hospital_Mgmnt_System.HospitalMAnagementSystsem.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.management.AttributeNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +26,7 @@ import com.hospital_Mgmnt_System.HospitalMAnagementSystsem.repository.PatientRep
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/patients")
 public class patientController {
 	
 	public patientController(PatientRepository patientRepository) {
@@ -33,9 +41,44 @@ public class patientController {
         return patientRepository.save(patient);
         }
 	
-	@GetMapping
+	@GetMapping("/fetch")
 	public List<Patient> getPatients()
 	{
 		return patientRepository.findAll();
+	}
+	
+	@SuppressWarnings("deprecation")
+	@GetMapping("/fetch/{id}")
+	public ResponseEntity<Patient> getPatientById(@PathVariable long id) throws AttributeNotFoundException
+	{
+		Patient patient = patientRepository.findById(id).orElseThrow(()-> new AttributeNotFoundException("Patient with "+ id + " number didn't exist in records"));
+		return ResponseEntity.ok(patient);
+	}
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Map<String, Boolean>> deletePatient(@PathVariable long id) throws AttributeNotFoundException
+	{
+		Patient patient = patientRepository.findById(id).orElseThrow(()-> new AttributeNotFoundException("Patient with "+ id + " number didn't exist in records"));
+		patientRepository.delete(patient);
+		Map<String, Boolean> response = new HashMap<String, Boolean>();
+		response.put("Deleted Patient", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Patient> updatePatientDetail(@PathVariable long id, @RequestBody Patient patientDetail) throws AttributeNotFoundException
+	{
+		Patient patient = patientRepository.findById(id).orElseThrow(()-> new AttributeNotFoundException("Patient with "+ id + " number didn't exist in records"));
+		patient.setAge(patientDetail.getAge());
+		patient.setBlood(patientDetail.getBlood());
+		patient.setDose(patientDetail.getDose());
+		patient.setFees(patientDetail.getFees());
+		patient.setId(patientDetail.getId());
+		patient.setName(patientDetail.getName());
+		patient.setPrescription(patientDetail.getPrescription());
+		patient.setUrgency(patientDetail.getUrgency());
+		
+		Patient savedPatient = patientRepository.save(patient);
+		return ResponseEntity.ok(savedPatient);
+		
 	}
  }
